37c5b0f3ee7e033f7fba94c8929d624ebeaf2904
Melissa Perenson <melissaperenson@Melissas-MacBook-Pro.local>
2015-07-11 22:55:05 -0700

    linked up blank menu pages and made universal

---
+++ PillMinder/IntervalViewController.h
@@ -0,0 +1,13 @@
+//
+//  IntervalViewController.h
+//  PillMinder
+//
+//  Created by Melissa Perenson on 7/11/15.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface IntervalViewController : UIViewController
+
+@end
---
+++ PillMinder/TimeremainingViewController.h
@@ -0,0 +1,13 @@
+//
+//  TimeremainingViewController.h
+//  PillMinder
+//
+//  Created by Melissa Perenson on 7/11/15.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TimeremainingViewController : UIViewController
+
+@end
